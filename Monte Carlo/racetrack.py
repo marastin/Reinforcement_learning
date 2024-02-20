1725ef71b2496aceaebfe7d7244fe2dd1fbfdf9c
marastin <58289836+marastin@users.noreply.github.com>
2024-02-20 02:33:34 +0100

    complete code

--- Monte Carlo/racetrack.py
+++ Monte Carlo/racetrack.py
@@ -2,6 +2,11 @@ import numpy as np
 import random
 import matplotlib.pyplot as plt
 
+Reward = -1
+Gamma = 1
+n_episodes = 10000
+
+
 # number of race track rows and columns
 margin = 5
 rows, cols = 32 + margin, margin + 17 + margin # 37, 27
@@ -19,6 +24,8 @@ Track[28, 6:21] = 1
 Track[29:31, 7:21] = 1
 Track[31, 8:21] = 1
 Track[26:32, 21:27] = 3  # finish line
+
+Track_visited = -Track.copy()
 
 # plt.figure(figsize=(10, 6))  # Adjust the figure size as needed
 # plt.imshow(Track, cmap='tab10', aspect='equal')
@@ -49,9 +56,10 @@ for v1 in range(n_vel):
                 tmp.append(idx)
         valid_actions.append(tmp)
 
-Q = np.random.rand([rows, cols, n_vel, n_vel, n_actions]) # action value 4-D matrix
-C = np.zeros([rows, cols, n_vel, n_vel, n_actions]) # cumulative weights for each state-action pair
-policy = np.ones((rows, cols, n_vel, n_vel), dtype=int) # policy matrix
+Q = np.random.rand(rows, cols, n_vel, n_vel, n_actions) * 400 - 500 # action value 4-D matrix
+C = np.zeros((rows, cols, n_vel, n_vel, n_actions)) # cumulative weights for each state-action pair
+policy = np.ones((rows, cols, n_vel, n_vel), dtype=int) # policy matrix (target)
+R = np.zeros((n_episodes, 1))
 
 for r in range(rows):
     for c in range(cols):
@@ -64,14 +72,13 @@ S = np.empty(10**6, dtype=tuple) # memory for state
 B = np.empty(10**6, dtype=int)   # memory for selected actions by behaviour policy
 P = np.empty(10**6, dtype=float) # memory for selected actions probabilities
 
-def generate_episode(epsilon):
-
+def generate_episode(epsilon = 0.1):
     t = 0
     S[0] = (0, random.choice(start_line), 0, 0)
     
     while True:
         s = S[t]
-        possible_actions = valid_actions[s[3]*n_vel + s[4]]
+        possible_actions = valid_actions[s[2]*n_vel + s[3]]
         n_possible_actions = len(possible_actions)
 
         a_policy = policy[*s] # action based on the target policy
@@ -90,8 +97,69 @@ def generate_episode(epsilon):
         
         B[t] = behavior
         P[t] = prob
-        act_b = actions(behavior)
+        act_b = actions[behavior]
         
+        vel = (s[2] + act_b[0], s[3] + act_b[1])
+        next_s = (s[0] + vel[0], s[1] + vel[1], vel[0], vel[1])
+
+        # Check being in finish area
+        if Track[next_s[0], next_s[1]] == 3:
+            return t+1
+        
+        # check hit the boundries
+        elif Track[next_s[0], next_s[1]] == 0:
+            s = (0, random.choice(start_line), 0, 0)
+        else:
+            s = next_s
+
+        t += 1
+        S[t] = s
+
+def update_policy(T):
+    G = 0
+    W = 1
+    
+    for t in range(T-1, -1, -1):
+        s = S[t]
+        a = B[t]
+        sa = (*s, a)
+
+        G = Reward + Gamma*G
+        C[sa] += W
+        Q[sa] += W * (G - Q[sa]) / C[sa]
+        
+        possible_actions = valid_actions[s[2]*n_vel + s[3]]
+        policy[s] = possible_actions[np.argmax(Q[s][possible_actions])]
+
+        Track_visited[s[0], s[1]] = 1
+        if policy[s] != a:
+            return
+        
+        W /= P[t]
+
+
+def main():
+    for i in range(n_episodes):
+        T = generate_episode(epsilon=0.1)
+        update_policy(T)    
+        R[i] = Reward*T
+        if (i+1) % 10 == 0:
+            print(f"episode {i}: {R[i]}")
+        
+    
+    plt.plot(R)
+    plt.show()
+
+    plt.figure(figsize=(10, 6))  # Adjust the figure size as needed
+    plt.imshow(Track_visited, aspect='equal')
+    plt.colorbar()  
+    plt.show()
+
+
+
+if __name__ == "__main__":
+    main()
+
 
 
 
