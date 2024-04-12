11ce2424d1605d4c8012306a4eebd5697b814e36
marastin <58289836+marastin@users.noreply.github.com>
2024-04-12 03:02:08 +0200

    separate buffer

--- DRL/DQN/Dueling_DQN_CartPole_v1.py
+++ DRL/DQN/Dueling_DQN_CartPole_v1.py
@@ -10,33 +10,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 
-
-# Named tuple for storing transitions
-Transition = namedtuple('Transition', ['state', 'action', 'reward', 'next_state', 'terminated', 'truncated'])
-
-class ReplayBuffer:
-    """Replay buffer to store transitions"""
-
-    def __init__(self, capacity):
-        """Initialize replay buffer"""
-        self.capacity = capacity
-        self.memory = []
-        self.position = 0
-
-    def push(self, *args):
-        """Add a transition to the replay buffer"""
-        if len(self.memory) < self.capacity:
-            self.memory.append(None)
-        self.memory[self.position] = Transition(*args)
-        self.position = (self.position + 1) % self.capacity
-
-    def sample(self, batch_size):
-        """Sample a batch of transitions from the replay buffer"""
-        return random.sample(self.memory, batch_size)
-
-    def __len__(self):
-        """Return the current size of the replay buffer"""
-        return len(self.memory)
+from buffer import ReplayBuffer
 
 class QNetwork(nn.Module):
     """Q-value network"""
