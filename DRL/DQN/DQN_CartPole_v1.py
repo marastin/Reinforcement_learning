c5656f983c26afbdcebea1cb474f1515cb86e07e
marastin <58289836+marastin@users.noreply.github.com>
2024-04-13 03:37:30 +0200

    minor modification

--- DRL/DQN/DQN_CartPole_v1.py
+++ DRL/DQN/DQN_CartPole_v1.py
@@ -11,24 +11,7 @@ import torch.nn.functional as F
 import torch.optim as optim
 from buffer import ReplayBuffer
 from networks import QNetwork
-
-def evaluation(env, q_network):
-    """Evaluate the agent's performance under greedy policy"""
-    state, info = env.reset()
-    total_reward = 0
-    
-    for _ in range(episode_max_length):
-        with T.no_grad():
-            q_values = q_network(T.tensor(state, dtype=T.float32).to(q_network.device))
-            action = q_values.argmax().item()
-        
-        next_state, reward, terminated, truncated, info = env.step(action)
-        total_reward += reward
-        state = next_state
-        if terminated or truncated:
-            break
-    return total_reward
-
+from utils import evaluation
 
 if __name__ == "__main__":
 
@@ -128,7 +111,7 @@ if __name__ == "__main__":
                     #     target.data.copy_(online.data)
             
             if terminated or truncated:
-                print(f"Episode {episode} | Total Reward: {total_reward} | Eps: {epsilon:.4f} | t: {t}")
+                print(f"Ep {episode:4.0f} | TR: {total_reward:4.0f} | ε: {epsilon:.4f} | t: {t}")
                 break
         
         
